bd537838963fa3189551bddc94a9c5428335d88e
Bill Lee <ood11611doo@gmail.com>
2026-01-13 10:24:59 +0900

    260113 - Initialization Initial upload

---
+++ Source/u_260122_Project006.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class u_260122_Project006Target : TargetRules
+{
+	public u_260122_Project006Target(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+		ExtraModuleNames.Add("u_260122_Project006");
+	}
+}
---
+++ Source/u_260122_Project006/u_260122_Project006.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "u_260122_Project006.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, u_260122_Project006, "u_260122_Project006" );
